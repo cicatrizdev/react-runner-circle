9ed9adf3efd90bb393ca51f7ddab130562a8a24c
Pedro Mello <contato@cicatriz.dev>
2025-08-21 17:50:20 -0300

    aula 4, video 4.2

--- src/pages/NewPost.jsx
+++ src/pages/NewPost.jsx
@@ -1,43 +1,43 @@
-import Header from '../components/layout/Header'
-import Sidebar from '../components/layout/Sidebar'
-import BottomNavigation from '../components/layout/BottomNavigation'
-import NewPostForm from '../components/forms/NewPostForm'
+import Header from '../components/layout/Header';
+import Sidebar from '../components/layout/Sidebar';
+import BottomNavigation from '../components/layout/BottomNavigation';
+import NewPostForm from '../components/forms/NewPostForm';
+import { useMutation } from '@apollo/client';
+import { ADD_FEED_POST } from '../../database/graphql/mutation/feed';
 
 function NewPost({ onNavigateToFeed }) {
-  const handleSubmit = (formData) => {
-    console.log('Nova postagem:', formData)
-    // Aqui seria onde salvaria os dados no backend
-    // Por enquanto, só navega de volta ao feed
-    onNavigateToFeed?.()
-  }
+	const [addFeedPost, { loading: savingPost }] = useMutation(ADD_FEED_POST);
+	const handleSubmit = (formData) => {
+		console.log('Nova postagem:', formData);
+		// Aqui seria onde salvaria os dados no backend
+		// Por enquanto, só navega de volta ao feed
+		onNavigateToFeed?.();
+	};
 
-  const handleCancel = () => {
-    onNavigateToFeed?.()
-  }
+	const handleCancel = () => {
+		onNavigateToFeed?.();
+	};
 
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <Header />
-      
-      <div className="flex">
-        {/* Desktop Sidebar */}
-        <Sidebar activeItem="feed" />
-        
-        {/* Main Content */}
-        <main className="flex-1 p-4 md:p-6 pb-20 md:pb-6">
-          <div className="max-w-4xl mx-auto">
-            <NewPostForm 
-              onSubmit={handleSubmit}
-              onCancel={handleCancel}
-            />
-          </div>
-        </main>
-      </div>
+	return (
+		<div className='min-h-screen bg-gray-50'>
+			<Header />
 
-      {/* Mobile Bottom Navigation */}
-      <BottomNavigation activeItem="feed" />
-    </div>
-  )
+			<div className='flex'>
+				{/* Desktop Sidebar */}
+				<Sidebar activeItem='feed' />
+
+				{/* Main Content */}
+				<main className='flex-1 p-4 md:p-6 pb-20 md:pb-6'>
+					<div className='max-w-4xl mx-auto'>
+						<NewPostForm onSubmit={handleSubmit} onCancel={handleCancel} />
+					</div>
+				</main>
+			</div>
+
+			{/* Mobile Bottom Navigation */}
+			<BottomNavigation activeItem='feed' />
+		</div>
+	);
 }
 
-export default NewPost
+export default NewPost;
